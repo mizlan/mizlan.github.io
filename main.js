429e25a743d9680abd9d03ad5bafd691abb806fc
Michael Lan <michaellan202@gmail.com>
2023-03-03 20:25:45 -0800

    recent track

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const resp = fetch('https://recenttrack-mizlan.vercel.app/api/handler').then((res) => res.json())
+
 window.onload = () => {
   const bongo = document.querySelector('.bongo')
   setInterval(() => {
@@ -6,4 +8,15 @@ window.onload = () => {
       bongo.style.removeProperty('transform');
     }, 300)
   }, 5000)
+
+  const track = document.querySelector('#track p')
+  resp.then((json) => {
+    const name = `<span class=".info">${json['name']}</span>`
+    const artist = `<span class=".info">${json['artist']['#text']}</span>`
+    if (json['@attr']?.['nowplaying'] == 'true') {
+      track.innerHTML = `Michael is currently listening to ${name} by ${artist}`
+    } else {
+      track.innerHTML = `Michael most recently listened to ${name} by ${artist}`
+    }
+  })
 }
